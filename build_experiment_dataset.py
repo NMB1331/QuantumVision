0d4fe5d6e8bb6cbc9843a62363aa8498a1db1551
unknown <natemburl@gmail.com>
2021-01-22 12:08:39 -0800

    Added predict file, some notes, and a gitignore

--- build_experiment_dataset.py
+++ build_experiment_dataset.py
@@ -2,5 +2,7 @@
 Contains functions for importing, formatting, and otherwise manipulating the dataset.
 
 Author: Nathaniel M. Burley
-Date Last Modified: 21st January 2021
+Date Created: 21st January 2021
+Last Modified By: Nathaniel M. Burley
+Date Last Modified: 22nd January 2021
 """
--- train_model.py
+++ train_model.py
@@ -1,6 +1,12 @@
 """
-This file contains functions for training the model
+This file contains functions for training the model.
+
+NOTE: https://www.pyimagesearch.com/2020/10/05/object-detection-bounding-box-regression-with-keras-tensorflow-and-deep-learning/
+NOTE: (Determining shapes of layers)
+https://towardsdatascience.com/a-guide-to-an-efficient-way-to-build-neural-network-architectures-part-ii-hyper-parameter-42efca01e5d7
 
 Author: Nathaniel M. Burley
-Date Last Modified: 21st January 2021
+Date Created: 21st January 2021
+Last Modified By: Nathaniel M. Burley
+Date Last Modified: 22nd January 2021
 """
