1844c18f9de903e51b4ff01264cfb831e6dc2855
unknown <natemburl@gmail.com>
2021-01-21 19:52:18 -0800

    Added file templates, architecture image, and requirements list

---
+++ experiment_dataset.py
@@ -0,0 +1,6 @@
+"""
+Contains functions for importing, formatting, and otherwise manipulating the dataset.
+
+Author: Nathaniel M. Burley
+Date Last Modified: 21st January 2021
+"""
---
+++ train_model.py
@@ -0,0 +1,6 @@
+"""
+This file contains functions for training the model
+
+Author: Nathaniel M. Burley
+Date Last Modified: 21st January 2021
+"""
