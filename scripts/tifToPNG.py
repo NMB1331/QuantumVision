c896f159ae117be096c58d9477db37e62a85121c
Nathan_B <54860453+FF-Brown@users.noreply.github.com>
2021-04-11 00:11:18 -0700

    Update tifToPNG.py
    Now includes GUI. Run without command line options to use GUI, or add source and directory folders to run directly.

--- scripts/tifToPNG.py
+++ scripts/tifToPNG.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[70]:
+# In[81]:
 
 
 from PIL import Image
 import numpy as np
 import os
 import sys
-
-"""
-Author: Nathan Brown
-
-Converts all .tif files in one folder to png files and saves them in another folder.
-Directory containing .tif files is first argument, destination is second.
-e.g. python tifToPNG.py C:\Users\YourName\Downloads\data_imgs\tests C:\Users\YourName\Downloads\data_imgs\tests\data
-"""
+import tkinter.filedialog
+import tkinter as tk
 
 def processTif(tifPath, tifName):
     """Convert a multipage tif to a folder of png files.
@@ -65,14 +59,62 @@ def processFolder(path, dest):
                 else:
                     source = os.path.join(root, name)
                     processTif(source, os.path.splitext(name)[0])
+                    
+class App(object):
+    def __init__(self, master):
+        frame = tk.Frame(master)
+        frame.pack()
+        self.text = tk.Text()
+        self.text.pack()
+ 
+        menu = tk.Menu(master)
+        root.config(menu=menu)
+        # file menu
+        filemenu = tk.Menu(menu, tearoff=0)
+        menu.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Set input folder", command=self.setInputFolder)
+        filemenu.add_command(label="Set output folder", command=self.setOutputFolder)   
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.do_exit)
+        
+        self.inputFolder = ""
+        self.outputFolder = ""
+        
+        self.setText("Input folder: " + self.inputFolder + "\nOutput folder: " + self.outputFolder)
+        
+    def setInputFolder():
+        filename = tk.filedialog.askopenfilename(title="Choose a Zoom chat file to open.", filetypes = [("Text Files", "*.txt"), ("All files", "*.*")])
+        temp = tk.filedialog.askdirectory(title="Choose source for tif files")
+        self.inputFolder = temp
+        self.setText("Input folder: " + self.inputFolder + "\nOutput folder: " + self.outputFolder)
+        
+    def setOutputFolder():
+        self.outputFolder = tk.filedialog.askdirectory(title="Choose destination for png files")
+        self.setText("Input folder: " + self.inputFolder + "\nOutput folder: " + self.outputFolder)
+
+    def setText(self, txt):
+        """Writes text to the editor window."""
+        if txt != None:
+            self.text.delete(0.0, tk.END)
+            self.text.insert(tk.END, txt) 
+
+    def do_exit(self):
+        root.destroy()
             
 if __name__ == "__main__":
     n = len(sys.argv)
-    print("{} arguments passed.".format(n))
+    if n >= 3:
+        print("{} arguments passed.".format(n))
+        print("Tif folder:", sys.argv[1])
+        print("PNG destination folder:", sys.argv[2])
+        
+        #processFolder(r"C:\Users\YourName\Downloads\data_imgs\tests", r"C:\Users\YourName\Downloads\data_imgs\tests\data")
+        #processFolder(sys.argv[1], sys.argv[2])
     
-    print("Tif folder:", sys.argv[1])
-    print("PNG destination folder:", sys.argv[2])
+        root = tk.Tk()
+        root.title("TIF to PNG Converter")
+        app = App(root)
+        root.mainloop()
+        
     
-    processFolder(sys.argv[1], sys.argv[2])
-    #processFolder(r"C:\Users\Nathan\Downloads\data_imgs\tests", r"C:\Users\Nathan\Downloads\data_imgs\tests\data")
 
